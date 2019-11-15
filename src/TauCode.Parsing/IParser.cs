fc12d3a2e94ef1ad48042f917b1b4e53d71863ba
Andrey Kovalenko <ak@taucode.com>
2019-11-15 22:33:12 +0200

    Continuing deleting.

--- src/TauCode.Parsing/IParser.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System.Collections.Generic;
-
-//namespace TauCode.Parsing
-//{
-//    public interface IParser
-//    {
-//        IContext Parse(IEnumerable<IToken> tokens);
-//    }
-//}
