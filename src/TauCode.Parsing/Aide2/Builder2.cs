9609ea92caebdbae3083018c93840b8294c66a3e
Andrey Kovalenko <ak@taucode.com>
2019-12-01 17:37:48 +0200

    Started working...

---
+++ src/TauCode.Parsing/Aide2/Builder2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TauCode.Parsing.TinyLisp;
+using TauCode.Parsing.TinyLisp.Data;
+
+namespace TauCode.Parsing.Aide2
+{
+    public class Builder2 : IBuilder2
+    {
+        private Dictionary<string, PseudoList> _defblocks;
+
+        public INode Build(PseudoList defblocks)
+        {
+            _defblocks = defblocks.ToDictionary(x => x.GetCarSymbolName(), x => x.AsPseudoList());
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/TauCode.Parsing.Tests/Building2/Builder2Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using TauCode.Parsing.Aide2;
+using TauCode.Parsing.Nodes;
+using TauCode.Parsing.TinyLisp;
+using TauCode.Utils.Extensions;
+
+namespace TauCode.Parsing.Tests.Building2
+{
+    [TestFixture]
+    public class Builder2Tests
+    {
+        [Test]
+        public void TodoWatBuild2()
+        {
+            // Arrange
+            var input = this.GetType().Assembly.GetResourceText("sql-grammar.lisp", true);
+            ILexer lexer = new TinyLispLexer();
+
+            var tokens = lexer.Lexize(input);
+
+            var reader = new TinyLispPseudoReader();
+            var list = reader.Read(tokens);
+
+            // Act
+            IBuilder2 builder = new Builder2();
+            var root = builder.Build(list);
+            root.FetchTree();
+
+            // Assert
+            Assert.That(root, Is.TypeOf<ExactWordNode>());
+            var exactWord = (ExactWordNode) root;
+            Assert.That(exactWord.Word, Is.EqualTo("CREATE"));
+
+            throw new NotImplementedException("good, go on!");
+        }
+    }
+}
