320db399702fdbb5082b115b54f44c1e9d20152a
Andrey Kovalenko <ak@taucode.com>
2020-01-15 01:10:48 +0200

    commented out.

--- src/TauCode.Parsing/TinyLisp/TinyLispLexer.cs
+++ src/TauCode.Parsing/TinyLisp/TinyLispLexer.cs
@@ -1,27 +1,30 @@
-﻿using System.Collections.Generic;
-using TauCode.Parsing.Lexing;
-using TauCode.Parsing.Lexing.StandardExtractors;
-using TauCode.Parsing.TinyLisp.TokenExtractors;
-using TauCode.Parsing.Tokens;
+﻿//using System.Collections.Generic;
+//using TauCode.Parsing.Lexing;
+//using TauCode.Parsing.Lexing.StandardExtractors;
+//using TauCode.Parsing.TinyLisp.TokenExtractors;
+//using TauCode.Parsing.Tokens;
 
-namespace TauCode.Parsing.TinyLisp
-{
-    public class TinyLispLexer : LexerBase
-    {
-        protected override IList<ITokenExtractor> CreateTokenExtractors()
-        {
-            return new ITokenExtractor[]
-            {
-                new TinyLispCommentExtractor(),
-                new TinyLispSymbolExtractor(),
-                new TinyLispPunctuationExtractor(),
-                new IntegerExtractor(new[]
-                {
-                    typeof(PunctuationToken),
-                }),
-                new TinyLispKeywordExtractor(),
-                new TinyLispStringExtractor(),
-            };
-        }
-    }
-}
+//namespace TauCode.Parsing.TinyLisp
+//{
+//    public class TinyLispLexer : LexerBase
+//    {
+//        protected override IList<ITokenExtractor> CreateTokenExtractors()
+//        {
+//            return new ITokenExtractor[]
+//            {
+//                new TinyLispCommentExtractor(),
+//                new TinyLispSymbolExtractor(),
+//                new TinyLispPunctuationExtractor(),
+//                new IntegerExtractor(new[]
+//                {
+//                    typeof(PunctuationToken),
+//                }),
+//                new TinyLispKeywordExtractor(),
+//                new TinyLispStringExtractor(),
+//            };
+//        }
+//    }
+//}
+
+
+// todo clean up
--- tests/TauCode.Parsing.Tests/Parsing/Sql/SqlLexer.cs
+++ tests/TauCode.Parsing.Tests/Parsing/Sql/SqlLexer.cs
@@ -1,25 +1,28 @@
-﻿using System.Collections.Generic;
-using TauCode.Parsing.Lexing;
-using TauCode.Parsing.Lexing.StandardExtractors;
-using TauCode.Parsing.Tests.Parsing.Sql.TokenExtractors;
-using TauCode.Parsing.Tokens;
+﻿//using System.Collections.Generic;
+//using TauCode.Parsing.Lexing;
+//using TauCode.Parsing.Lexing.StandardExtractors;
+//using TauCode.Parsing.Tests.Parsing.Sql.TokenExtractors;
+//using TauCode.Parsing.Tokens;
 
-namespace TauCode.Parsing.Tests.Parsing.Sql
-{
-    public class SqlLexer : LexerBase
-    {
-        protected override IList<ITokenExtractor> CreateTokenExtractors()
-        {
-            return new List<ITokenExtractor>
-            {
-                new WordExtractor(),
-                new SqlPunctuationExtractor(),
-                new IntegerExtractor(new[]
-                {
-                    typeof(PunctuationToken),
-                }),
-                new SqlIdentifierExtractor(),
-            };
-        }
-    }
-}
+//namespace TauCode.Parsing.Tests.Parsing.Sql
+//{
+//    public class SqlLexer : LexerBase
+//    {
+//        protected override IList<ITokenExtractor> CreateTokenExtractors()
+//        {
+//            return new List<ITokenExtractor>
+//            {
+//                new WordExtractor(),
+//                new SqlPunctuationExtractor(),
+//                new IntegerExtractor(new[]
+//                {
+//                    typeof(PunctuationToken),
+//                }),
+//                new SqlIdentifierExtractor(),
+//            };
+//        }
+//    }
+//}
+
+
+// todo clean up
