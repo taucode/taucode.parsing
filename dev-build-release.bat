5a1dee29a388340f47497e1f1a452cea82d5a520
ak <ak@taucode.com>
2020-03-15 22:57:44 +0200

    Packed locally.

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -6,4 +6,4 @@ dotnet build --configuration Release
 dotnet test -c Debug .\tests\TauCode.Parsing.Tests\TauCode.Parsing.Tests.csproj
 dotnet test -c Release .\tests\TauCode.Parsing.Tests\TauCode.Parsing.Tests.csproj
 
-nuget pack nuget\TauCode.Data.nuspec
+nuget pack nuget\TauCode.Parsing.nuspec
