f1a87461792c45aded6d91e230b5d86c587150d2
ak <ak@taucode.com>
2020-03-15 22:53:46 +0200

    Init bump.

---
+++ dev-build-release.bat
@@ -0,0 +1,9 @@
+dotnet restore
+
+dotnet build --configuration Debug
+dotnet build --configuration Release
+
+dotnet test -c Debug .\tests\TauCode.Parsing.Tests\TauCode.Parsing.Tests.csproj
+dotnet test -c Release .\tests\TauCode.Parsing.Tests\TauCode.Parsing.Tests.csproj
+
+nuget pack nuget\TauCode.Data.nuspec
